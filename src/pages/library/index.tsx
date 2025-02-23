87e0a26d0c54bdf299572ad58916e67d4cc2562e
Raihan Hafiz <dtp.hook627@gmail.com>
2025-02-23 14:12:11 +0000

    Fix Library Page

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,18 +1,20 @@
-// Library Page 02/22/2025 Raihan Hafiz
-import { useState, useEffect } from 'react';
-import AssetCard from '../../components/AssetCard';
-import AssetModal from '../../components/AssetModal';
+// Library Page(Main Page)
+// Date: 02/22/2025
+// Author: Raihan Hafiz
+import { useState, useEffect } from "react";
+import AssetCard from "../../components/AssetCard";
+import AssetModal from "../../components/AssetModal";
 import CreateAssetModal from "../../components/CreateAssetModal";
-import RequestAssetModal from '@/components/RequestAssetModal';
-import axios from 'axios';
+import RequestAssetModal from "@/components/RequestAssetModal";
+import axios from "axios";
 import kpiData from "../../models/kpi_data";
-import DataVizModal from '@/components/DataVizModal';
-import StoryboardModal from '@/components/StoryboardModal';
+import DataVizModal from "@/components/DataVizModal";
+import StoryboardModal from "@/components/StoryboardModal";
 
 export default function LibraryPage() {
   const [assets, setAssets] = useState<any[]>([]);
   const [trendAssets, setTrendAssets] = useState<any[]>([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedAsset, setSelectedAsset] = useState<any>(null);
   const [selectedKPI, setSelectedKPI] = useState<any>(null);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
@@ -35,21 +37,19 @@ export default function LibraryPage() {
       fetchTrendAssets();
     }
   }, [searchQuery]);
-  
 
   const fetchAssets = async () => {
     try {
       const res = await axios.get(`/api/assets`);
       const data = res.data;
-  
 
-      const filteredAssets = data.filter(asset =>
+      const filteredAssets = data.filter((asset) =>
         asset.name.toLowerCase().includes(searchQuery.toLowerCase())
       );
-  
+
       setAssets(filteredAssets);
     } catch (error) {
-      console.error('Error fetching assets:', error);
+      console.error("Error fetching assets:", error);
     }
   };
 
@@ -58,7 +58,7 @@ export default function LibraryPage() {
       const res = await axios.get(`/api/assets?search=`);
       setTrendAssets(res.data);
     } catch (error) {
-      console.error('Error fetching assets:', error);
+      console.error("Error fetching assets:", error);
     }
   };
 
@@ -67,8 +67,10 @@ export default function LibraryPage() {
   };
 
   const handleTrendingCardClick = (asset: any) => {
-    setSelectedTrendingAsset(asset._id === selectedTrendingAsset?._id ? null : asset);
-    setSelectedFeaturedAsset(null); 
+    setSelectedTrendingAsset(
+      asset._id === selectedTrendingAsset?._id ? null : asset
+    );
+    setSelectedFeaturedAsset(null);
     setActiveTab("Featured");
   };
 
@@ -78,12 +80,11 @@ export default function LibraryPage() {
       fetchKPIData(selectedAsset.name);
     } else if (tab === "Data Viz" && selectedAsset) {
       setIsDataVizModalOpen(true);
-    } else if ( tab === "Layout" && selectedAsset ) {
+    } else if (tab === "Layout" && selectedAsset) {
       setIsModalOpen(true);
-    } else if ( tab === "Storyboard"  && selectedAsset) {
+    } else if (tab === "Storyboard" && selectedAsset) {
       setIsStoryboardOpen(true);
-    };
-
+    }
   };
 
   const fetchKPIData = (assetName: string) => {
@@ -97,21 +98,49 @@ export default function LibraryPage() {
   return (
     <div className="max-w-5xl px-48 mx-auto bg-gray-200 p-6 rounded-lg shadow-md">
       <div className="absolute top-6 right-96 px-24 flex gap-4">
-        <button onClick={() => setIsCreateModalOpen(true)} className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition">Create</button>
-        <button onClick={() => setIsRequestModalOpen(true)} className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition">Request</button>
+        <button
+          onClick={() => setIsCreateModalOpen(true)}
+          className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition"
+        >
+          Create
+        </button>
+        <button
+          onClick={() => setIsRequestModalOpen(true)}
+          className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition"
+        >
+          Request
+        </button>
       </div>
 
       <h1 className="text-5xl text-center font-bold mb-8">Library</h1>
-      <input type="text" className="w-full border rounded p-2 mb-4" placeholder="Type to search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+      <input
+        type="text"
+        className="w-full border rounded p-2 mb-4"
+        placeholder="Type to search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
 
       <div className="flex bg-gray-100 p-2 rounded-lg mb-4">
         {["Featured", "KPI", "Data Viz", "Layout", "Storyboard"].map((tab) => (
-          <button key={tab} className={`w-full px-6 py-2 text-sm font-medium rounded-md ${activeTab === tab ? "bg-white shadow text-gray-900" : "text-gray-500 hover:text-gray-700"}`} onClick={() => handleTabClick(tab)}>{tab}</button>
+          <button
+            key={tab}
+            className={`w-full px-6 py-2 text-sm font-medium rounded-md ${
+              activeTab === tab
+                ? "bg-white shadow text-gray-900"
+                : "text-gray-500 hover:text-gray-700"
+            }`}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </button>
         ))}
       </div>
-     
+
       <h1 className="font-bold text-3xl mb-3 w-full">{activeTab}</h1>
-      <h1 className="text-sm mb-5 w-full text-gray-600 ">Created top picks from this week</h1>
+      <h1 className="text-sm mb-5 w-full text-gray-600 ">
+        Created top picks from this week
+      </h1>
 
       {activeTab === "KPI" && selectedKPI ? (
         <div className="bg-white p-4 rounded-lg shadow">
@@ -120,9 +149,15 @@ export default function LibraryPage() {
             <div key={kpi.id} className="mb-4 border p-3 rounded-lg">
               <h3 className="text-lg font-semibold">{kpi.name}</h3>
               <p>{kpi.description}</p>
-              <p><strong>Metric ID:</strong> {kpi.metricID}</p>
-              <p><strong>Calculation:</strong> {kpi.calculation}</p>
-              <p><strong>Visuals:</strong> {kpi.visualsAvailable.join(", ")}</p>
+              <p>
+                <strong>Metric ID:</strong> {kpi.metricID}
+              </p>
+              <p>
+                <strong>Calculation:</strong> {kpi.calculation}
+              </p>
+              <p>
+                <strong>Visuals:</strong> {kpi.visualsAvailable.join(", ")}
+              </p>
             </div>
           ))}
         </div>
@@ -133,28 +168,39 @@ export default function LibraryPage() {
             <div key={kpi.id} className="mb-4 border p-3 rounded-lg">
               <h3 className="text-lg font-semibold">{kpi.name}</h3>
               <p>{kpi.description}</p>
-              <p><strong>Metric ID:</strong> {kpi.metricID}</p>
-              <p><strong>Calculation:</strong> {kpi.calculation}</p>
-              <p><strong>Visuals:</strong> {kpi.visualsAvailable.join(", ")}</p>
+              <p>
+                <strong>Metric ID:</strong> {kpi.metricID}
+              </p>
+              <p>
+                <strong>Calculation:</strong> {kpi.calculation}
+              </p>
+              <p>
+                <strong>Visuals:</strong> {kpi.visualsAvailable.join(", ")}
+              </p>
             </div>
           ))}
         </div>
-      ) : activeTab !== "Layout" && activeTab !== "Storyboard" && (
+      ) : (
+        activeTab !== "Layout" &&
+        activeTab !== "Storyboard" && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
             {assets.map((asset) => (
-              <AssetCard 
-                key={asset._id} 
-                asset={asset} 
-                refreshAssets={fetchAssets} 
-                onClick={() => handleAssetClick(asset)} 
-                isSelected={selectedAsset && selectedAsset._id === asset._id} 
+              <AssetCard
+                key={asset._id}
+                asset={asset}
+                refreshAssets={fetchAssets}
+                onClick={() => handleAssetClick(asset)}
+                isSelected={selectedAsset && selectedAsset._id === asset._id}
               />
             ))}
           </div>
+        )
       )}
 
       <h1 className="font-bold text-3xl  w-full py-6">Trending</h1>
-      <h6 className="text-sm mb-4 w-full text-gray-600">Most popular by community</h6>
+      <h6 className="text-sm mb-4 w-full text-gray-600">
+        Most popular by community
+      </h6>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
         {trendAssets.map((trendAsset) => (
           <AssetCard
@@ -162,7 +208,10 @@ export default function LibraryPage() {
             asset={trendAsset}
             refreshAssets={fetchAssets}
             onClick={() => handleTrendingCardClick(trendAsset)}
-            isSelected={selectedTrendingAsset && selectedTrendingAsset._id === trendAsset._id}
+            isSelected={
+              selectedTrendingAsset &&
+              selectedTrendingAsset._id === trendAsset._id
+            }
           />
         ))}
       </div>
@@ -173,7 +222,7 @@ export default function LibraryPage() {
           onClose={() => setIsDataVizModalOpen(false)}
         />
       )}
-   
+
       {isModalOpen && selectedAsset && (
         <AssetModal
           asset={selectedAsset}
@@ -187,17 +236,25 @@ export default function LibraryPage() {
       {isStoryboardOpen && selectedAsset && (
         <StoryboardModal
           asset={selectedAsset}
-
           onClose={() => {
             setIsStoryboardOpen(false);
             setSelectedAsset(null);
           }}
-
         />
       )}
 
-      {isRequestModalOpen && <RequestAssetModal onClose={() => setIsRequestModalOpen(false)} refreshAssets={fetchAssets} />}
-      {isCreateModalOpen && <CreateAssetModal onClose={() => setIsCreateModalOpen(false)} refreshAssets={fetchAssets} />}
+      {isRequestModalOpen && (
+        <RequestAssetModal
+          onClose={() => setIsRequestModalOpen(false)}
+          refreshAssets={fetchAssets}
+        />
+      )}
+      {isCreateModalOpen && (
+        <CreateAssetModal
+          onClose={() => setIsCreateModalOpen(false)}
+          refreshAssets={fetchAssets}
+        />
+      )}
     </div>
   );
 }
